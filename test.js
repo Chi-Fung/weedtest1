5e5c7f0fa471b1cf3db7241dddf8bbd1c0cb786a
Chi-Fung <73382691+Chi-Fung@users.noreply.github.com>
2022-04-19 04:09:09 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,81 @@
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = 'https://chi-fung.github.io/weedtest1/';
 
-let img;
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+let confidence = "";
+let weed = "";
 
+// Load the model first
 function preload() {
- 
-  img = loadImage('IMG_0514.JPG');
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  img = loadImage('kitten.jpg');
 
 }
 
 function setup() {
-  createCanvas(500, 500);
-  img.resize(300,400);
+  createCanvas(320, 320);
+  // Create the video
+/*   var constraints = {
+	audio : false,
+	video : {
+		facingMode : {
+			exact: "environment"
+		}
+	}
+  }; 
+  video = createCapture(VIDEO);
+  video.size(320, 320);
+  video.hide();*/
+  img.resize(200,200);
+
+  flippedVideo = ml5.flipImage(img)
+  // Start classifying
+ // classifyVideo();
+}
+
+function draw() {
+  background(0);
+  // Draw the video
   image(img, 0, 0);
 
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text("label:" + label + "  confidence:" + confidence + "  " + weed, width / 2, height-4);
+  
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  confidence = nf(results[0].confidence, 0, 2);
+	
+  if(confidence>0.7){
+	weed = "雜草";	
+  }else{
+	weed = "  ";  
+  }
+  
+ 
+  classifyVideo();
 }
